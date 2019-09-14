05f42b381994dac511cef2bb62f19a7712bf7a43
Webatics <femmor2u@gmail.com>
2019-09-14 16:48:02 -0500

    finished the menu-item and the directory listings

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Components
+import Directory from "../../components/directory/directory.component";
+
+// Styles
+import "./homepage.styles.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
